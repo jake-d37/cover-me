6f06c1770087d1e9bb4395a425efc5304b0e0a41
Jake Andrade <jdeandrade@student.unimelb.edu.au>
2025-04-30 11:27:25 +1000

    initial commit. form implemented. no functionality

---
+++ src/App.tsx
@@ -0,0 +1,237 @@
+import { ButtonHTMLAttributes, useState } from 'react'
+import { useEffect } from 'react';
+import React from 'react';
+import { Plus } from 'lucide-react';
+import './App.css'
+
+interface PdfSelection {
+    
+}
+
+function App() {
+    //LATER: default these from local storage
+    const [fname, setFirstName] = useState<string>("");
+    const [lname, setLastName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [phone, setPhoneNumber] = useState<string>("");
+
+    //LATER: default this too
+    const [resume, setResume] = useState(); //PDF type when I put in the library
+
+    const [jobLink, setJobLink] = useState<string>("");
+    const [jobDesc, setJobDescription] = useState<string>("");
+
+    const toneOptions: readonly string[] = [
+        "Professional",
+        "Enthusiastic",
+        "Analytical",
+        "Creative",
+        "Collaborative",
+        "Leadership-oriented",
+        "Mission-driven",
+        "Adaptable",
+        "Customer-focused",
+        "Technical",
+        "Strategic",
+        "Transitional",
+        "Growth-oriented",
+        "Industry-savvy",
+        "Global-minded"
+      ];;
+
+    //LATER: default this too
+    const [tonesSelected, setToneSelected] = useState<Set<string>>(new Set());
+
+    const [notes, setNotes] = useState<string>("");
+
+    const [coverLetterContent, setCoverLetter] = useState<string>("");
+
+    //just to check if its working
+    useEffect(()=> {
+        console.log(fname);
+    }, [fname]);
+
+    const handleToneSelection = (tone: string) => {
+        setToneSelected(prev => {
+            const newSet = new Set(prev);
+            if (newSet.has(tone)) {
+              newSet.delete(tone);
+            } else {
+              newSet.add(tone);
+            }
+            console.log("Tones selected: ", newSet);
+            return newSet;
+        });
+    };
+
+    
+
+    const findJobDescription = (e: React.MouseEvent<HTMLButtonElement>, link: string) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        //check its public/legal to scrape
+
+        //scrape
+    };
+
+    const generateCoverLetter = (e: React.MouseEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+        e.preventDefault();
+        //concatenate everything
+
+        //give to LLM
+
+        //return result
+        setCoverLetter("Whatever DeepSeek said");
+    }
+
+    const exportCoverLetter = (e: React.MouseEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+        e.preventDefault();
+        //print the cover letter script to pdf using jsPDF
+    }
+  
+    return (
+        <>
+            <h1>CoverMe</h1>
+            <div className='form-body'>
+                <form className='form-body'>
+                    <div className='basic-info'>
+                        <label className='form-field'>
+                            First Name:
+                            <input 
+                                type="text" 
+                                name="fname"
+                                placeholder='Jane'
+                                value={fname}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            >
+                            </input>
+                        </label>
+                        <label className='form-field'>
+                            Last Name:
+                            <input 
+                                type="text" 
+                                name="lname"
+                                placeholder='Citizen'
+                                value={lname}
+                                onChange={(e) => setLastName(e.target.value)}
+                            >
+                            </input>
+                        </label>
+                        <label className='form-field'>
+                            Email:
+                            <input 
+                                type="text" 
+                                name="email"
+                                placeholder='janecitizen@email.com'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </input>
+                        </label>
+                        <label className='form-field'>
+                            Phone Number:
+                            <input 
+                                type="text" 
+                                name="phone"
+                                placeholder='0400 123 456'
+                                value={phone}
+                                onChange={(e) => {
+                                    const value = e.target.value;
+                                    // Allow only digits
+                                    if (/^\d*$/.test(value)) {
+                                        setPhoneNumber(value);
+                                    }
+                                }}
+                            >
+                            </input>
+                        </label>
+                    </div>
+                    <div className='form-field'>
+                        RESUME SELCTOR GOES HERE
+                        
+                    </div>
+                    <label className='form-field'>
+                        Job description link: 
+                        <input 
+                            type="text" 
+                            name="joblink"
+                            placeholder='Link to job page goes here...'
+                            value={jobLink}
+                            onChange={(e) => setJobLink(e.target.value)}
+                        >
+                        </input>
+                        <button
+                            onClick={(e) => findJobDescription(e, jobLink)}
+                        >
+                            Retrieve job description
+                        </button>
+                    </label>
+                    <label className='form-field'>
+                        Job description: 
+                        <textarea 
+                            name="jobdesc"
+                            value={jobDesc}
+                            placeholder='...or paste job description here'
+                            onChange={(e) => setJobDescription(e.target.value)}
+                        >
+                        </textarea>
+                    </label>
+                    <div className='tones-box'>
+                        <div className='form-field'>Writing style</div>
+                        {toneOptions.map(option => (
+                            <label key={option} className='form-field'>
+                                <input 
+                                    type='checkbox'
+                                    checked={tonesSelected.has(option)}
+                                    value={option}
+                                    onChange={() => handleToneSelection(option)}
+                                />
+                                {option}
+                            </label>
+                        ))}
+                    </div>
+                    <label className='form-field'>
+                        Added notes:
+                        <input
+                            type="text"
+                            name="notes"
+                            value={notes}
+                            placeholder='Any other requests here, in natural language'
+                            onChange={(e) => setNotes(e.target.value)}
+                        >
+                        </input>
+                    </label>
+                    <input 
+                        className='form-field'
+                        type="submit" 
+                        value="Generate cover letter" 
+                        onClick={(e) => generateCoverLetter(e)}
+                    />
+                </form> 
+                <form className='form-body'>
+                    <label className='form-field'>
+                        Edit cover letter result. We recommend removing em dashes--these, for example.
+                        <textarea
+                            className='form-field'
+                            name="cover-letter-contenets"
+                            value={coverLetterContent}
+                            onChange={(e) => setCoverLetter(e.target.value)}
+                        >
+                        </textarea>
+                    </label>
+                    <input
+                        className='form-field'
+                        type="submit"
+                        value="Export as PDF"
+                        onClick={(e)=>exportCoverLetter(e)}
+                    ></input>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default App
